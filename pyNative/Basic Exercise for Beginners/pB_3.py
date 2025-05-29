bd95f62e92e94bcf266bb9785413288138eb7655
rsrattan <rsrattan.education@gmail.com>
2025-05-29 19:07:32 +0530

    pB_3

--- pyNative/Basic Exercise for Beginners/pB_3.py
+++ pyNative/Basic Exercise for Beginners/pB_3.py
@@ -1,4 +1,4 @@
 word = input("Input a String: ")
-print("Orginal String is  PYnative\nPrinting only even index chars")
+print(f"Orginal String is  {word}\nPrinting only even index chars")
 for i in range(0, len(word), 2):
     print(word[i])
